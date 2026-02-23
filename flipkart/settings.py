6d1392d8152aeb9053126b7d8d3cb007c320e75d
AshOP31244 <ashwajpoojary2@gmail.com>
2026-02-23 12:27:30 +0530

    Fix cloudinary config

--- flipkart/settings.py
+++ flipkart/settings.py
@@ -62,6 +62,14 @@ CLOUDINARY_STORAGE = {
     'API_SECRET': os.environ.get('CLOUDINARY_API_SECRET'),
 }
 
+cloudinary.config(
+    cloud_name=os.environ.get('CLOUDINARY_CLOUD_NAME'),
+    api_key=os.environ.get('CLOUDINARY_API_KEY'),
+    api_secret=os.environ.get('CLOUDINARY_API_SECRET'),
+)
+
+CLOUDINARY_URL = os.environ.get("CLOUDINARY_URL")
+
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
 
